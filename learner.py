73712f43ddbc0e7b137435a147924445614c994a
undercutspiky <undercutspiky@yahoo.co.in>
2015-12-22 02:52:40 +0100

    Update learner.py

--- learner.py
+++ learner.py
@@ -11,6 +11,7 @@ class learner():
         self.learnerID = learnerID
         self.learnedValues = {}
         self.mcast_groups = mcast_groups
+        self.printed = 1
         
         mcast_grp = mcast_groups["learners"]
         sock_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -30,26 +31,45 @@ class learner():
             t = threading.Thread(target = self.listen, args=(sock,sock_s))
             t.start()
             t.join()
-    
+            self.printValues()
+        
     def listen(self, sock, sock_s):
         
-        mcast_grp = mcast_groups["learners"]
+        mcast_grp = self.mcast_groups["learners"]
         
         data, address = sock.recvfrom(1024)
-        #sent = sock_s.sendto(data, mcast_grp)
         arr = cp.loads(data)
         
         if len(arr) == 2:
-            self.receiveValue(arr[0], arr[1])
+            if arr[0] not in self.learnedValues.keys():
+                sent = sock_s.sendto(data, mcast_grp)
+                sent = 0
+                self.receiveValue(arr[0], arr[1], sock_s)
+        elif len(arr) == 1:
+            sq_number = arr[0]
+            if sq_number in self.learnedValues.keys():
+                sent = sock_s.sendto(cp.dumps([sq_number, self.learnedValues[sq_number]]), mcast_grp)
         else:
             print 'Invalid data received from proposer'
     
-    def receiveValue(self, sequenceNumber, acceptedValue):
+    def receiveValue(self, sequenceNumber, acceptedValue, sock_s):
+        #Check if all previous sequence numbers are present else ask learners for values
+        mcast_grp = self.mcast_groups["learners"]
+        for i in xrange(1,sequenceNumber):
+            if i not in self.learnedValues.keys():
+                sent = sock_s.sendto(cp.dumps([i]), mcast_grp)
+                
         if sequenceNumber not in self.learnedValues.keys():
             self.learnedValues[sequenceNumber] = acceptedValue
-        #print "LEARNED %s FOR %s" % (acceptedValue,sequenceNumber)
-        print acceptedValue
-        sys.stdout.flush()
+    
+    def printValues(self):
+        i = self.printed
+        while i in self.learnedValues.keys():
+            print self.learnedValues[i]
+            i += 1
+            self.printed += 1
+            sys.stdout.flush()
+        
 
 if __name__ == '__main__':
     learnerID = random.randint(1,5000)
